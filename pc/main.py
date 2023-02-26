8d50103850cbdc0d6b1224b1ce37bbd3a7c9b094
Yuxuan Zhu <yuxuanz3@andrew.cmu.edu>
2023-02-25 23:11:56 -0500

    made some folder

---
+++ pc/main.py
@@ -0,0 +1,23 @@
+import socket
+import struct
+
+# Create UDP socket.
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to BeamNG OutGauge.
+sock.bind(('127.0.0.1', 4444))
+
+while True:
+	# Receive data.
+	data = sock.recv(96)
+
+	if not data:
+		break # Lost connection
+
+	# Unpack the data.
+	outsim_pack = struct.unpack('I4sH2c7f2I3f16s16si', data)
+	
+	print("RPM: ", str(outsim_pack[6])) 
+	
+# Release the socket.
+sock.close()
---
+++ pc/pyinput.py
@@ -0,0 +1,8 @@
+import pyxinput
+
+def test():
+    pyxinput.test_virtual()
+
+
+if __name__ == "__main__":
+    test()
