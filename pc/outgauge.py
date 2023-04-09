606d4ab1ffcd0cb15d2fc097edc9ece2e1e37e07
Samaritan0923 <51545925+Samaritan0923@users.noreply.github.com>
2023-04-08 20:57:21 -0400

    updated outgauge, can read data

--- pc/outgauge.py
+++ pc/outgauge.py
@@ -16,7 +16,7 @@ while True:
 	# Receive data.
 	print("Receiving data")
 
-	data = sock.recv(96)
+	data = sock.recv(1024)
 	print("Data received")
 	
 	if not data:
@@ -26,9 +26,25 @@ while True:
 	# Unpack the data.
 	outsim_pack = struct.unpack('I4sH2c7f2I3f16s16si', data)
 	print(outsim_pack)
+	play_time = int(outsim_pack[0])
+	name = str(outsim_pack[1])
+	gear = str(outsim_pack[3])
+	speed = float(outsim_pack[5]) #M/S
+	rpm = float(outsim_pack[6])
+	turbo = float(outsim_pack[7])
+	engineTemp = float(outsim_pack[8])
+
+	result = {}
+	result["play_time"] = play_time
+	result["name"] = name
+	result["gear"] = gear
+	result["speed"] = speed
+	result["rpm"] = rpm
+	result["turbo"] = turbo
+	result["engineTemp"] = engineTemp
+	print(result)
 	
-	time.sleep(1)
-	print("test")
+	# time.sleep(1)
 	
 # Release the socket.
 sock.close()
